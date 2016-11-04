afcafd2342f5b248df86cede8826d349aa35cab4
Philipp <philipp.bode@student.hpi.de>
2016-11-04 14:26:46 +0100

    Added author adding, solves #10

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,6 +1,19 @@
 class AuthorsController < ApplicationController
 
 	def new
+		@authors = Author.all
 		render "new"
 	end
+
+	def create
+	  @author = Author.new(author_params)
+	  @author.save
+
+	  render "new"
+	end
+	 
+	private
+	  def author_params
+	    params.require(:author).permit(:first_name, :last_name, :homepage)
+	  end
 end
--- spec/features/author_spec.rb
+++ spec/features/author_spec.rb
@@ -1,14 +1,15 @@
-feature 'User creates an Author' do
-	scenario 'they fill in the form' do 
-	visit new_author_path
+require 'rails_helper'
 
-	fill_in 'First name', with: 'Alan'
-	fill_in 'Last name', with: 'Turing'
-	fill_in 'Homepage', with: 'http://wikipedia.org/wiki/Alan_Turing'
+describe "Author new test", :type => :feature do
+	it "should create a new author" do
+		visit new_author_path
 
-	click_button 'Create Author'
+		fill_in 'First name', with: 'Alan'
+		fill_in 'Last name', with: 'Turing'
+		fill_in 'Homepage', with: 'http://wikipedia.org/wiki/Alan_Turing'
 
-	expect Author.find(first_name: 'Alan', last_name: 'Turing', homepage: 'http://wikipedia.org/wiki/Alan_Turing').to exist
+		click_button 'Create Author'
 
+		expect(Author.where(first_name: "Alan", last_name: "Turing", homepage: "http://wikipedia.org/wiki/Alan_Turing")).to exist
 	end
 end
