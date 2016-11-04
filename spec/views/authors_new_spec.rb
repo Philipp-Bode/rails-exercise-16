ba4016107af20361626b33326897b573b0fae16e
Philipp <philipp.bode@student.hpi.de>
2016-11-04 11:50:07 +0100

    Reading issue titles helps..

--- spec/views/authors_new_spec.rb
+++ spec/views/authors_new_spec.rb
@@ -2,5 +2,8 @@ describe "authors/new" do
 	it "displays a new author form" do
 		render template: "authors/new.html.erb"
 		expect(rendered).to match /first name/
+		expect(rendered).to match /last name/
+		expect(rendered).to match /homepage/
+
 	end
 end
