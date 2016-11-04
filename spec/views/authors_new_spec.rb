dcb3a74eca8d38ec672943775873001236bec302
Philipp <philipp.bode@student.hpi.de>
2016-11-04 11:58:59 +0100

    Now with proper tests for #6

--- spec/views/authors_new_spec.rb
+++ spec/views/authors_new_spec.rb
@@ -1,9 +1,12 @@
+require "capybara/rspec"
+
 describe "authors/new" do
 	it "displays a new author form" do
 		render template: "authors/new.html.erb"
-		expect(rendered).to match /first name/
-		expect(rendered).to match /last name/
-		expect(rendered).to match /homepage/
+
+		expect(rendered).to have_field('First name')
+		expect(rendered).to have_field('Last name')
+		expect(rendered).to have_field('Homepage')
 
 	end
 end
