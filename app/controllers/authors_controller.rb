697759686a2206af5c4c263ed6045e4934ae113b
Philipp <philipp.bode@student.hpi.de>
2016-11-04 19:13:08 +0100

    Closes #22

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -22,6 +22,10 @@ class AuthorsController < ApplicationController
 	def show
 		@author = Author.find(params[:id])
 	end
+
+	def edit
+		@author = Author.find(params[:id])
+	end
 	 
 	private
 	  def author_params
