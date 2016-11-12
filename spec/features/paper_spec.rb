b7545492a32b8872f689632f663034edf5919566
Philipp <philipp.bode@student.hpi.de>
2016-11-12 17:46:21 +0100

    Failing #34

--- spec/features/paper_spec.rb
+++ spec/features/paper_spec.rb
@@ -14,4 +14,17 @@ describe "New Paper page", :type => :feature do
 
   	expect(Paper.where(title: "COMPUTING MACHINERY AND INTELLIGENCE", venue: "Mind 49: 433-460", year: 1950)).to exist
   end
+
+  it "should update a paper" do
+  	@paper = create(:paper)
+
+  	visit edit_paper_path(@paper)
+
+  	fill_in 'Title', with: 'TDD: COMPUTING MACHINERY AND INTELLIGENCE'
+
+
+  	click_button 'Update Paper'
+
+  	expect(Paper.find(@paper.id).title).to eq 'TDD: COMPUTING MACHINERY AND INTELLIGENCE'
+  end
 end
