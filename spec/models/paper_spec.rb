6022dd76aeae8badd617cea059fa8ee96ad81fbc
Philipp <philipp.bode@student.hpi.de>
2016-11-12 16:58:13 +0100

    Failing for #28

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  	it "should not be valid without a last name" do
+			build(:paper, title: "").should_not be_valid
+		end
 end
