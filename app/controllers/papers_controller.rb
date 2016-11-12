023b7780bf2fa6d699767539a851a200bfdafc39
Philipp <philipp.bode@student.hpi.de>
2016-11-12 17:17:45 +0100

    Closes #30

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -5,4 +5,16 @@ class PapersController < ApplicationController
 
 		render "new"
 	end
+
+	def create
+	  @paper = Paper.new(paper_params)
+	  @paper.save
+
+	  render "new"
+	end
+
+	private
+	  def paper_params
+	    params.require(:paper).permit(:title, :venue, :year)
+	  end
 end
