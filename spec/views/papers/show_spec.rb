f133fafb4789cfacef4b184ace45f46a057eb5f5
Philipp <philipp.bode@student.hpi.de>
2016-11-12 18:24:43 +0100

    Failing #37

--- spec/views/papers/show_spec.rb
+++ spec/views/papers/show_spec.rb
@@ -5,11 +5,14 @@ describe "papers/:id" do
 	it "displays paper details" do
 		@paper = create(:paper)
 
+		@paper.authors << create(:author)
+
 		render template: "papers/show.html.erb"
 
 		expect(rendered).to match /COMPUTING MACHINERY AND INTELLIGENCE/
 		expect(rendered).to match /Mind 49: 433-460/
 		expect(rendered).to match /1950/
+		expect(rendered).to match /Alan Turing/
 
 	end
 end
