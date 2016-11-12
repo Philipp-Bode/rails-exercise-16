ed8eb129dbda3db7bc50dca0d23a2630dd20170e
Philipp <philipp.bode@student.hpi.de>
2016-11-12 17:05:38 +0100

    Failing #29

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,7 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  	it "should not be valid without a last name" do
+  	it "should not be valid without a title" do
 			build(:paper, title: "").should_not be_valid
 		end
+
+		it "should not be valid without a venue" do
+			build(:paper, venue: "").should_not be_valid
+		end
+
+		it "should not be valid without a year" do
+			build(:paper, year: nil).should_not be_valid
+		end
+
 end
