a9564ddeba689ddf61a163a8be68a577e257ddb1
Philipp <philipp.bode@student.hpi.de>
2016-11-12 18:13:37 +0100

    Failing #36

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
+
   	it "should not be valid without a title" do
 			build(:paper, title: "").should_not be_valid
 		end
@@ -11,6 +12,11 @@ RSpec.describe Paper, type: :model do
 
 		it "should not be valid with a non-integer year" do
 			build(:paper, year: 'Early 19th century').should_not be_valid
+		end
+
+		it "should have a list of authors" do
+			@paper = create(:paper)
+			expect(@paper.authors).to eq []
 		end
 
 end
